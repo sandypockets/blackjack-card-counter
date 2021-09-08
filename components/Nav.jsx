63436f3736803fdf46725e9033d629e63571d5c3
sandypockets <zac@znacreative.com>
2021-09-08 16:31:10 -0400

    Style title

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,7 +1,7 @@
 export default function Nav() {
   return (
     <nav className="h-16 w-screen flex justify-center">
-      <h1 className="text-4xl pt-3">Blackjack Card Counter</h1>
+      <h1 className="text-4xl font-extrabold pt-3">Blackjack Card Counter</h1>
     </nav>
   )
 }
